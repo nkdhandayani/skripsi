8147b59a32b63f06627c06c9e691f4c8c850977a
Komang Krisnanda <officialkrisnandha@gmail.com>
2021-01-17 15:58:45 +0800

    Show profile name on master layout

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -68,7 +68,13 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="{{url('adminLTE/dist/img/user2-160x160.jpg')}}" class="user-image" alt="User Image">
-              <span class="hidden-xs">Alexander Pierce</span>
+              <span class="hidden-xs">
+                @if(\Auth::guard('user')->check())
+                  {{ \Auth::guard('user')->user()->nm_user }}
+                @elseif(\Auth::guard('bpw')->check())
+                  {{ \Auth::guard('bpw')->user()->nm_bpw }}
+                @endif
+              </span>
             </a>
 
             <ul class="dropdown-menu">
@@ -77,8 +83,11 @@
                 <img src="{{url('adminLTE/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
 
                 <p>
-                  Alexander Pierce - Web Developer
-                  <small>Member since Nov. 2012</small>
+                  @if(\Auth::guard('user')->check())
+                    {{ \Auth::guard('user')->user()->nm_user }}
+                  @elseif(\Auth::guard('bpw')->check())
+                    {{ \Auth::guard('bpw')->user()->nm_bpw }}
+                  @endif
                 </p>
               </li>
 
